3374497257946c84f18143bff9b9699c3e926ac4
Ed Schiffer <edschiffer@gmail.com>
2015-01-12 18:12:17 -0200

    Developed First JQuery Interface by the Template Project Master

--- www/js/index.js
+++ www/js/index.js
@@ -34,6 +34,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        FastClick.attach(document.body);
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
