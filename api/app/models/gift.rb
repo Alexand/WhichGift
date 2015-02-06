cd4778d7019eaa22092876bfe84bfad7ef757c7d
JoaoAlexandre <joao.contact@icloud.com>
2015-02-05 22:20:14 -0200

    Sets Images sizes to 100x100 256x256 512x512 1024x1024

--- api/app/models/gift.rb
+++ api/app/models/gift.rb
@@ -4,8 +4,9 @@ class Gift < ActiveRecord::Base
 
 	has_attached_file :photo, styles: {
     thumb: '100x100>',
-    square: '200x200#',
-    medium: '300x300>'
+    medium: '256x256#',
+    large: '512x512>',
+    giant: '1024x1024>'
   }
 
 	validates_attachment_file_name :photo, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
